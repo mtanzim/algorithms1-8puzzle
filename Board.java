8370d58b1c5b12b101a45b945698257f576e8aad
Tanzim Mokammel <mtanzim@gmail.com>
2019-03-31 22:51:18 +0200

    add twin method to board

--- Board.java
+++ Board.java
@@ -7,8 +7,6 @@
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdOut;
 
-// @edu.umd.cs.findbugs.annotations.SuppressFBWarnings("UUF_UNUSED_FIELD")
-// @edu.umd.cs.findbugs.annotations.SuppressFBWarnings("UUF_UNUSED_FIELD")
 public class Board {
 
     private int[][] tiles;
@@ -16,8 +14,19 @@ public class Board {
     private int n;
 
     public Board(int[][] blocks) {
-        tiles = blocks.clone();
+        tiles = deepCopyBoard(blocks);
         n = tiles.length;
+    }
+
+    // shallow copy on multidimensional arrays with clone
+    // thus each subarray needs its own clone
+    // https://stackoverflow.com/questions/184710/what-is-the-difference-between-a-deep-copy-and-a-shallow-copy
+    private int[][] deepCopyBoard(int[][] oldBoard) {
+        int[][] newBoard = new int[oldBoard.length][];
+        for (int i = 0; i < oldBoard.length; i++) {
+            newBoard[i] = oldBoard[i].clone();
+        }
+        return newBoard;
     }
 
     public int dimensions() {
@@ -49,6 +58,33 @@ public class Board {
     }
 
     public Board twin() {
+
+        int x1 = -1;
+        int y1 = -1;
+        int x2 = -1;
+        int y2 = -1;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (tiles[i][j] == 0) continue;
+                if (x1 == -1 && y1 == -1) {
+                    x1 = i;
+                    y1 = j;
+                    // StdOut.println("a is " + tiles[i][j] + " at: " + x1 + " " + y1);
+                    continue;
+                }
+                x2 = i;
+                y2 = j;
+                // StdOut.println("b is " + tiles[i][j] + " at: " + x2 + " " + y2);
+                break;
+            }
+            if (x2 > -1 && y2 > -1) break;
+        }
+
+        int[][] twin = deepCopyBoard(tiles);
+        twin[x1][y1] = tiles[x2][y2]; // b
+        twin[x2][y2] = tiles[x1][y1]; // a
+        return new Board(twin);
+
 
     }
 
@@ -86,6 +122,10 @@ public class Board {
             StdOut.println(board.toString());
             StdOut.println(board.isGoal());
             StdOut.println(board.hamming());
+            StdOut.println("TWIN");
+            StdOut.println(board.twin().toString());
+            StdOut.println("ORIG");
+            StdOut.println(board.toString());
             // Solver solver = new Solver(initial);
             // StdOut.println(filename + ": " + solver.moves());
         }
